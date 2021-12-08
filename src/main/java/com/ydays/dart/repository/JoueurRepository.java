748e85731ad17c7a8092567588df23fac8aaa432
Anthony Bastard <abastard05@orange.fr>
2021-12-08 14:33:48 +0100

    Oubli

--- src/main/java/com/ydays/dart/repository/JoueurRepository.java
+++ src/main/java/com/ydays/dart/repository/JoueurRepository.java
@@ -2,6 +2,9 @@ package com.ydays.dart.repository;
 
 import com.ydays.dart.model.Joueur;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JoueurRepository extends CrudRepository<Joueur, Long> {
+
 }
